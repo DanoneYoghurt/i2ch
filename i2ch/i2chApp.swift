7f611b707cd5129c8a6806891d2c20d416563b27
Anton Baskakov <antonbask@gmail.com>
2024-07-20 11:40:15 +0300

    Initial Commit

---
+++ i2ch/i2chApp.swift
@@ -0,0 +1,17 @@
+//
+//  i2chApp.swift
+//  i2ch
+//
+//  Created by Антон Баскаков on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct i2chApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
